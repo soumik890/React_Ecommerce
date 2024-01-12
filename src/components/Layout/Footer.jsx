cfcc24558183599ea07ec9b85ac8c2a1d4114b45
Soumik <soumik890@gmail.com>
2024-01-13 02:40:37 +0530

    test

---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light p-3">
+      <h4 className="text-center">
+        All Rights Reserved &copy;
+        <span className="fs-6">Soumik Chakraborty</span>
+      </h4>
+    </div>
+  );
+};
+
+export default Footer;
