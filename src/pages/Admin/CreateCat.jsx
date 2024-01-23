e162ae6b563a54d153371e8f537eeee25744aaf4
Soumik <soumik890@gmail.com>
2024-01-23 21:01:08 +0530

    test

--- src/pages/Admin/CreateCat.jsx
+++ src/pages/Admin/CreateCat.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layout/Layout";
 import AdminMenu from "../../components/Layout/AdminMenu";
+import toast from "react-hot-toast";
+import axios from "axios";
 const CreateCat = () => {
+  const [categories, setCategories] = useState([]);
+
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_APP_API}/api/v1/category/get-category`
+      );
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something wwent wrong in getting catgeory");
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
   return (
     <Layout
       title={"Create category-Ecom App"}
@@ -15,7 +36,32 @@ const CreateCat = () => {
             <AdminMenu />
           </div>
           <div className="col-md-9">
-            <h1>Create Category</h1>
+            <h1>Manage Category</h1>
+
+            <div>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {categories.map((item, key) => {
+                    return (
+                      <>
+                        <tr>
+                          <td key={key}>{item.name}</td>
+                          <td key={key}>
+                            <button className="btn btn-primary">Edit</button>
+                          </td>
+                        </tr>
+                      </>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
